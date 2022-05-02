9d031b8cf415d9c148df70029f20ebab47872cca
Metallifax <mathew_forgione@msn.com>
2022-05-02 02:41:02 -0300

    initial + first try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from './components/container/Container';
+import RatingContent from './components/rating/RatingContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-mediumGrey grid place-items-center h-screen'>
+      <Container>
+        <RatingContent />
+      </Container>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      orange: 'hsl(25, 97%, 53%)',
+      black: 'hsl(0, 0%, 6%)',
+      white: 'hsl(0, 0%, 100%)',
+      lightGrey: 'hsl(217, 12%, 63%)',
+      mediumGrey: 'hsl(216, 12%, 54%)',
+      darkBlue: 'hsl(213, 19%, 18%)',
+      veryDarkBlue: 'hsl(216, 12%, 8%)',
+    },
+    fontFamily: {
+      sans: ['Overpass', 'sans-serif'],
+    },
+    extend: {
+      'gradient-radial': 'radial-gradient(var(darkBlue, veryDarkBlue))'
+    },
+  },
+  plugins: [],
+};
